e68696ee6e5b39af82661054c7f01cd6839604c5
Nikhil Patkar's Research Group at ACTREC, Tata Memorial Center <nvpatkar@gmail.com>
2022-07-20 09:33:05 +0530

    Files_related_to_PCGR_CPSR

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -7,11 +7,7 @@
 
 ##########
 #for ENTRY : BEDFILES#
-##for LEUKEMIA: 06112021_Leukemia_Panel_sorted
-##for MIPS: myeloidrp_coordinates_file
-##for AMPLICON [SRSF2/GATA2/U2AF2]: USG
-##for AMPLICON [KDM]: ablnew
-##for CLL: cll_coordinates_file
-##for ALL: ALL_coordinates_byexons
-##for MRD: 04243058_MRD_Panel_V1
-nextflow -c /scratch/hematopath/hemoseq_v2/NextSeq_mutation_detector_leukemia/nextflow.config run main_v3.nf -entry MIPS --bedfile /scratch/hematopath/hemoseq_v2/mutation_detector_nextflow/bedfile/06112021_Leukemia_Panel_sorted --sequences /scratch/hematopath/hemoseq_v2/NextSeq_mutation_detector_leukemia/sequences_26Feb/ --input /scratch/hematopath/hemoseq_v2/NextSeq_mutation_detector_leukemia/samplesheet.csv -resume
+##for LEUKEMIA/MIPS: 06112021_Leukemia_Panel_sorted
+##for MIPS (IDT-MRD): /home/pipelines/mutation_detector_nextflow/bedfile/04243058_MRD_Panel_V1_final_sorted 
+##for CNVpanel+ALP:ALP_CNV_backbone_sorted.bed
+nextflow -c /home/pipelines/NextSeq_mutation_detector_leukemia/nextflow.config run main_v3_pcgr.nf -entry MIPS --bedfile /home/pipelines/mutation_detector_nextflow/bedfile/06112021_Leukemia_Panel_sorted --sequences /home/pipelines/NextSeq_mutation_detector_leukemia/sequences/ --input /home/pipelines/NextSeq_mutation_detector_leukemia/samplesheet.csv -resume -bg
